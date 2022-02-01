37583dd4780c08080436485dc7bf81d94cba0352
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 00:38:45 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,12 +1,13 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
 import streamlit as st
-PAGE_CONFIG = {"page_title":"StColab.io","page_icon":":smiley:","layout":"centered"}
-st.beta_set_page_config(**PAGE_CONFIG)
-def main():
-	st.title("Awesome Streamlit for ML")
-	st.subheader("How to run streamlit from colab")
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox('Menu',menu)
-	if choice == 'Home':
-		st.subheader("Streamlit From Colab")	
-if __name__ == '__main__':
-	main()
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
