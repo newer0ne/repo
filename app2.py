f64868df50bfdafa6a306850d63bb3b7e56055ee
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 12:15:22 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,13 +5,12 @@ Here's our first attempt at using data to create a table:
 
 import streamlit as st
 import pandas as pd
-import openpyxl
+import openpyxl as
 
 uploaded_file = st.file_uploader("Загрузка Excel")
 if uploaded_file is not None:
-     book = pd.read_excel(uploaded_file, read_only = true, engine='openpyxl')
-     sheet = book.active
-     st.write(sheet)
+     book = pd.read_excel(uploaded_file)
+     st.write(book)
     
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
