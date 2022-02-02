6764f9ef602e437dc1b1f6f9a979ae4bcb9895c2
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 14:51:16 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-
 uploaded_file = st.file_uploader("Загрузка Excel")
 if uploaded_file is not None:
      dataframe = pd.read_excel(uploaded_file)
-     dataframe = dataframe[('Примечание')]
      st.write(dataframe)
 
 df = pd.DataFrame({'first column': [1, 2, 3, 4], 
