df27251cee0cf03409c229bfa51369f9fba30879
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 17:10:26 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ import io
 
 uploaded_file = st.file_uploader("Загрузка Excel")
 if uploaded_file is not None:
-     dataframe = pd.read_excel(io='uploaded_file', 
+     dataframe = pd.ExcelFile.parse(io='uploaded_file', 
                              engine='openpyxl',
                              sheet_name='Sheet1',
                              skiprows=2,
