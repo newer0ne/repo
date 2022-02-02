af8006ce7e6fda585c6a59e50f69f32f06e2e384
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 11:37:34 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,13 +3,14 @@
 Here's our first attempt at using data to create a table:
 """
 
+import streamlit as st
+import pandas as pd
+
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
     
-import streamlit as st
-import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40],
