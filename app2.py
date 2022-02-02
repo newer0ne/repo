29ba6e953945302ed9c16292a10e6c8fcb0d72c6
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 23:07:03 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,13 +8,13 @@ import numpy as np
 X = st.file_uploader("Загрузка Excel")
 if X is not None:
      print(X)
-     excel_workbook = xlrd.open_workbook(X)
-     excel_worksheet = excel_workbook.sheet_by_index(1) # Открывает первый лист, 1 - второй и т.д.
-     df = pd.DataFrame(excel_worksheet)
-     df1 = df.drop([0, 1], axis=0)
-     df1.columns = df1.iloc[0]
-     df2 = df1.drop([2])
-     st.write(df2)
+#     excel_workbook = xlrd.open_workbook(X)
+#     excel_worksheet = excel_workbook.sheet_by_index(1) # Открывает первый лист, 1 - второй и т.д.
+#     df = pd.DataFrame(excel_worksheet)
+#     df1 = df.drop([0, 1], axis=0)
+#     df1.columns = df1.iloc[0]
+#     df2 = df1.drop([2])
+#     st.write(df2)
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
                    'second column': [10, 20, 30, 40], 
