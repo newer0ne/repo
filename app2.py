a8adb6b9317ab8ac0c0c894f16a33f283e59e8ef
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 11:40:39 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,8 +10,7 @@ uploaded_file = st.file_uploader("Загрузка Excel")
 if uploaded_file is not None:
      dataframe = pd.read_excel(uploaded_file)
      st.write(dataframe)
-
-dataframe
+     dataframe
     
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
