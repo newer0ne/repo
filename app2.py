8b3a7e68b65c5065b00818d7ea7844dfc7eb26aa
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 17:14:24 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,12 +5,12 @@ import io
 uploaded_file = st.file_uploader("Загрузка Excel")
 if uploaded_file is not None:
      dataframe = pd.ExcelFile.parse(io='uploaded_file', 
-                             engine='openpyxl',
+                             engine='openpyxl')
                              #sheet_name='Sheet1',
                              #skiprows=2,
                              #usecols='C:K',
                              #nrows=10,
-                                   )  
+ 
      st.write(dataframe)
 
 df = pd.DataFrame({'first column': [1, 2, 3, 4], 
