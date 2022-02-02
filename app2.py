68391f3ab8462beb0ba6736fbba8e2b843182e91
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-02 11:20:25 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,8 @@ import streamlit as st
 import pandas as pd
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'second column': [10, 20, 30, 40],
+  'third column': ['ебаный', 'рот', 'этого', 'казино']
 })
 
 df
