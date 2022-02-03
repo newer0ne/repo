b96aded7277f892f7b70792c5471f6f799f57809
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 10:27:37 +0300

    Create app.py

--- app.py
+++ app.py
@@ -8,21 +8,17 @@ import csv
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате CSV", type='csv')
 if uploaded_file is not None:
-     with open(uploaded_file, 'r', encoding='utf-8', errors='ignore') as infile, open(uploaded_file + 'final.csv', 'w') as outfile:
+     with open(uploaded_file, 'r') as infile:
           inputs = csv.reader(infile)
-          output = csv.writer(outfile)
-
-          for index, row in enumerate(inputs):
-           # Create file with no header
-               if index == 0:
-                    continue
-               output.writerow(row)
+        
+          for line in enumerate(inputs):
+               print(line)
      
-     df = pd.read_csv(outfile)
-     df1 = df.drop([0, 1], axis=0)
-     df1.columns = df1.iloc[0]
-     df2 = df1.drop([2])
-     st.write(df2)
+#     df = pd.read_csv(outfile)
+#     df1 = df.drop([0, 1], axis=0)
+#     df1.columns = df1.iloc[0]
+#     df2 = df1.drop([2])
+#     st.write(df2)
 
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
