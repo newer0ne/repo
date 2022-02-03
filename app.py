6024e87c81de39091390f3129143ea910d497305
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 10:38:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,14 @@ import pandas as pd
 import numpy as np
 import csv
 
-uploaded_file = st.file_uploader("Зафгрузка файла в формате CSV", type='csv')
-if uploaded_file is not None:
-     with open(uploaded_file, 'r') as infile:
-          inputs = csv.reader(infile)
-        
-          for line in enumerate(inputs):
-               print(line)
-     
-#     df = pd.read_csv(outfile)
+uploaded_file = st.file_uploader("Зафгрузка файла в формате xlsx", type='xlsx')
+if uploaded_file is not None:   
+     df = pd.read_excel(uploaded_file)
 #     df1 = df.drop([0, 1], axis=0)
 #     df1.columns = df1.iloc[0]
 #     df2 = df1.drop([2])
 #     st.write(df2)
+     st.write(df)
 
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
