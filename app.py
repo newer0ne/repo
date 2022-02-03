df93b51ec2c12c03aeae842bd55d6aa05ba89a84
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 10:48:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import streamlit as st
 import io
 import os
 import xlrd
+from openpyxl import load_workbook
 import pandas as pd
 import numpy as np
 import csv
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате xlsx", type='xlsx')
 if uploaded_file is not None:   
-     df = pd.read_excel(uploaded_file)
+     book = load_workbook(uploaded_file)
+     sheet = book.active
+#     df = pd.read_excel(uploaded_file)
 #     df1 = df.drop([0, 1], axis=0)
 #     df1.columns = df1.iloc[0]
 #     df2 = df1.drop([2])
 #     st.write(df2)
-     st.write(df)
+     st.write(sheet)
 
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
