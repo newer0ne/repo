9cb1585b1374cb2e3b83658b126461d5ce4b49cb
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 11:17:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ import csv
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file is not None:   
-     pd.read_excel(uploaded_file)
+#     pd.read_excel(uploaded_file)
 #     df1 = df.drop([0, 1], axis=0)
 #     df1.columns = df1.iloc[0]
 #     df2 = df1.drop([2])
 #     st.write(df2)
-#     st.write(df)
+     st.write(pd.read_excel(uploaded_file))
 
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
