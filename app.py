2c959317e0f5deaad228df05194b2e8ff531cc45
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 11:59:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ import io
 import os
 import csv
 
-uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
-if uploaded_file is not None:   
-     df1 = pd.read_excel(uploaded_file)
-     df1 = pd.DataFrame(df, columns=['KKS Code','Note'])
-     st.write(df1)
+uploaded_file_1 = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
+if uploaded_file_1 is not None:   
+     df1 = pd.read_excel(uploaded_file_1)
+     df11 = pd.DataFrame(df, columns=['KKS Code','Note'])
+     st.write(df11)
 
 uploaded_file_2 = st.file_uploader("Зафгрузка 2 файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file_2 is not None:   
      df2 = pd.read_excel(uploaded_file_2)
-     df2 = pd.DataFrame(df, columns=['KKS Code','Note'])
-     st.write(df2)
+     df22 = pd.DataFrame(df, columns=['KKS Code','Note'])
+     st.write(df22)
 
 df3 = pd.DataFrame({'first column': [1, 2, 3, 4], 
                    'second column': [10, 20, 30, 40], 
