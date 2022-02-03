52dc32a64a8965b571b508ce36e6214df8d6f940
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-03 11:47:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import csv
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file is not None:   
-     df = pd.read_excel(uploaded_file, index_col='KKS Code')
-     df1 = pd.DataFrame(df['KKS Code','Note']).values.ravel()
+     df = pd.read_excel(uploaded_file)
+     df1 = pd.DataFrame(df, columns=['KKS Code','Note'])
 #     df1.columns = df1.iloc[0]
 #     df2 = df1.drop([2])
 #     st.write(df2)
