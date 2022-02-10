c832350b2a6cf11a27a55f729f93755632a0482b
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 20:26:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ tab = pd.DataFrame(rows)
 #st.write(tab)
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
-if uploaded_file is not None:   
-     A = pd.read_excel(uploaded_file)
-#     excel_1 = pd.DataFrame(A, columns=['Note', 'KKS Code'])
-final = pd.merge(A, tab, how = 'inner', on = ['Note']) 
-st.write(final)
+if uploaded_file is not None:
+    A = pd.read_excel(uploaded_file)
+#    excel_1 = pd.DataFrame(A, columns=['Note', 'KKS Code'])
+    final = pd.merge(A, tab, how = 'inner', on = ['Note']) 
+    st.write(final)
