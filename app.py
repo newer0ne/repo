0444339a3392943b491552a2c1cdd7a4fa0f8f7f
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 20:33:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ if uploaded_file is not None:
     @st.cache
     def convert_df(df):
      # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
+#        return df.to_csv().encode('utf-8')
+        return df.to_excel()
 
-    csv = convert_df(final)
-    st.download_button(label="Download data as CSV", data=csv, file_name='final.csv', mime='text/csv')
+    output = convert_df(final)
+    st.download_button(label="Download data as CSV", data=output, file_name='output.csv')#, mime='text/csv')
