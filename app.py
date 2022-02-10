8d972c64712a0c850e28dd728636d56faf23ff13
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 19:43:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,8 @@ import io
 import os
 import csv
 
-uploaded_file_1 = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
-if uploaded_file_1 is not None:   
-     df1 = pd.read_excel(uploaded_file_1)
-#     df11 = pd.DataFrame(df1, columns=['Note', 'KKS Code'])
-     st.write(df1)
-
 # Create a connection object.
 conn = connect()
-
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
@@ -25,6 +18,11 @@ def run_query(query):
 
 sheet_url = st.secrets["public_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
 tab = pd.DataFrame(rows)
 st.write(tab)
+
+uploaded_file_1 = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
+if uploaded_file_1 is not None:   
+     df = pd.read_excel(uploaded_file_1)
+     df1 = pd.DataFrame(df1, columns=['Note', 'KKS Code'])
+     st.write(df1)
