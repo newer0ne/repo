3f592a5a755dfef0b6060dc7943a57b2b2dc1303
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 19:23:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import io
 import os
 import csv
 
-# .streamlit/secrets.toml
-
-public_gsheets_url = "https://docs.google.com/spreadsheets/d/1peUU7SHhShwFGF2cdpclgr3xJOJii2OQ/edit#gid=2119343756"
-
 uploaded_file_1 = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file_1 is not None:   
      df1 = pd.read_excel(uploaded_file_1)
