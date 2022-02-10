c3ea4595c1403a1e9d4afea9598267774e570cd6
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 21:03:20 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,20 +9,6 @@ import os
 import csv
 from pyxlsb import open_workbook as open_xlsb
 
-# Create a connection object.
-conn = connect()
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-tab = pd.DataFrame(rows)
-#st.write(tab)
-
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file is not None:
     A = pd.read_excel(uploaded_file)
