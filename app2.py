c30a1bbadc5deb84dbee8bf5701c7de1f0fc8a88
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 21:31:34 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,10 +11,11 @@ from pyxlsb import open_workbook as open_xlsb
 
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file is not None:
-    B = pd.read_excel(uploaded_file, sheet_name=1)
+    B = pd.read_excel(uploaded_file, sheet_name="Sheet1")
     C = pd.read_excel(uploaded_file, sheet_name=2)
     D = pd.read_excel(uploaded_file, sheet_name=3)
     E = pd.read_excel(uploaded_file, sheet_name=4)
+    st.write(B)
     st.write(C)
     st.write(D)
     st.write(E)
