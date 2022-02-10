5377acbc673e46598e232468ed8ad9a1b5e8b75c
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 19:54:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,13 @@ if uploaded_file_1 is not None:
 #     df1 = pd.DataFrame(df1, columns=['Note', 'KKS Code'])
 final = pd.merge(df, tab, how = 'inner', on = ['Note']) 
 st.write(final)
+
+@st.cache
+ def convert_df(df):
+     # IMPORTANT: Cache the conversion to prevent computation on every rerun
+     return df.to_csv().encode('utf-8')
+
+csv = convert_df(my_large_df)
+
+st.download_button(
+     label="Download data as CSV", data=csv, file_name='large_df.csv', mime='text/csv',)
