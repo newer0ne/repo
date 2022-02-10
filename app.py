0f8295476ef558a57b162e4ec3317e85c7f976ce
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 19:28:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from gsheetsdb import connect
 import pandas as pd
 import numpy as np
 import openpyxl
@@ -11,3 +12,16 @@ if uploaded_file_1 is not None:
      df1 = pd.read_excel(uploaded_file_1)
 #     df11 = pd.DataFrame(df1, columns=['Note', 'KKS Code'])
      st.write(df1)
+
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
