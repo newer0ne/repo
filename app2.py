f502284684723b4efc612072c9f99a2372fb2dfa
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-10 21:11:15 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,6 +12,18 @@ from pyxlsb import open_workbook as open_xlsb
 uploaded_file = st.file_uploader("Зафгрузка файла в формате .xlsx .xls .odf, .ods, .odt")
 if uploaded_file is not None:
     A = pd.read_excel(uploaded_file)
-    st.write(A)
+    def to_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+        format1 = workbook.add_format({'num_format': '0.00'})
+        worksheet.set_column('A:A', None, format1)
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+    df_xlsx = to_excel(A)
+    st.write(df_xlsx)
 
     
