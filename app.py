4a378adc433d701d206b31fe810bba6de9747f1d
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 23:25:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ if uploaded_file is not None:
     st.write(show_final)
     @st.cache
     
+        # Скачиваем обработанную ведомость
     def to_excel(df):
         output = BytesIO()
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
@@ -45,24 +46,24 @@ if uploaded_file is not None:
     df_xlsx = to_excel(final)
     st.download_button(label='📥 Скачать обработанную ведомость', data=df_xlsx, file_name= 'Ведомость опор.xlsx')
 
-# Загружаем таблицу опор Lisega 49 type
+    # Загружаем таблицу опор Lisega 49 type
 Li = st.secrets["public_gsheets_url_Lisega"]
 rows_Li = run_query(f'SELECT Dn, Lisega, Fz FROM "{Li}"')
 tab_Li = pd.DataFrame(rows_Li)
 tab_Li = tab_Li.astype({'Dn': float, 'Lisega': str, 'Fz': float})
 
-# Загружаем таблицу опор kt2 21 type
+    # Загружаем таблицу опор kt2 21 type
 sheet_url_t21 = st.secrets["public_gsheets_url_t21"]
 rows_21 = run_query(f'SELECT Dn, Fz_21, mark_21 FROM "{sheet_url_t21}"')
 tab_21 = pd.DataFrame(rows_21)
 tab_21 = tab_21.astype({'Dn': float, 'mark_21': str, 'Fz_21': float})
 
-# Загружаем таблицу опор kt2 31 type
+    # Загружаем таблицу опор kt2 31 type
 sheet_url_t31 = st.secrets["public_gsheets_url_t31"]
 rows_31 = run_query(f'SELECT Dn, Fz_31, mark_31 FROM "{sheet_url_t31}"')
 tab_31 = pd.DataFrame(rows_31)
 
-#st.write('Таблица соответствия опор Lisega 49 - kt2 21 type')
+    #st.write('Таблица соответствия опор Lisega 49 - kt2 21 type')
 tab_Li_kt21 = pd.merge(tab_Li, tab_21, how = 'inner', on = ['Dn'])
 tab_Li_kt21.dropna(subset=['Fz'], inplace=True)
 tab_Li_kt21 = tab_Li_kt21[tab_Li_kt21['Fz'] <= tab_Li_kt21['Fz_21']]
@@ -70,7 +71,7 @@ tab_Li_kt21 = tab_Li_kt21[tab_Li_kt21['Fz'] <= tab_Li_kt21['Fz_21']]
 #st.write(len(tab_Li_kt21))
 tab_Li_kt21_drop = tab_Li_kt21[['Lisega','mark_21','Fz_21']]
 
-#st.write('Таблица соответствия опор Lisega 49 - kt2 31 type')
+    #st.write('Таблица соответствия опор Lisega 49 - kt2 31 type')
 tab_Li_kt31 = pd.merge(tab_Li, tab_31, how = 'inner', on = ['Dn'])
 tab_Li_kt31.dropna(subset=['Fz'], inplace=True)
 tab_Li_kt31 = tab_Li_kt31[tab_Li_kt31['Fz'] <= tab_Li_kt31['Fz_31']]
@@ -78,7 +79,7 @@ tab_Li_kt31 = tab_Li_kt31[tab_Li_kt31['Fz'] <= tab_Li_kt31['Fz_31']]
 #st.write(len(tab_Li_kt31))
 tab_Li_kt31_drop = tab_Li_kt31[['Lisega','mark_31', 'Fz_31']]
 
-st.write('Таблица соответствия опор Lisega 49 - kt2 21, 31 type')
+    #st.write('Таблица соответствия опор Lisega 49 - kt2 21, 31 type')
 tab_Li_fin = pd.merge(tab_Li, tab_Li_kt21_drop, how = 'outer', on = ['Lisega'])
 tab_Li_fin = pd.merge(tab_Li_fin, tab_Li_kt31_drop, how = 'outer', on = ['Lisega'])
 #st.write(tab_Li_fin)
