434a54606d366ddc1390190068d918c3f2745be1
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 22:01:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ tab_31 = pd.DataFrame(rows_31)
 
 tab_Li_kt21 = pd.merge(tab_Li, tab_21, how = 'inner', on = ['Dn'])
 tab_Li_kt21.dropna(subset=['Fz'], inplace=True)
-count(tab_Li_kt21['Lisega'])
+len(tab_Li_kt21)
 
 st.write(tab_Li_kt21)
 tab_Li_kt21 = tab_Li_kt21[tab_Li_kt21['Fz'] <= tab_Li_kt21['Fz_21']]
