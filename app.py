d261feab7249dca424b3bb0dc5877adae136c497
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 21:03:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,7 @@ tab_Li_kt2 = pd.merge(tab_Li, tab_21, how = 'inner', on = ['Dn'])
 tab_Li_kt2 = pd.merge(tab_Li_kt2, tab_31, how = 'inner', on = ['Dn'])
 tab_Li_kt2.dropna(subset=['Fz'], inplace=True)
 st.write(tab_Li_kt2)
-for Fz_31 in tab_Li_kt2:
-    if tab_Li_kt2['Fz'] < tab_Li_kt2['Fz_21']:
+    if tab_Li['Fz'] < tab_21['Fz_21']:
         tab_Li_kt2['mark'] == 'yepp'
     else:
         tab_Li_kt2['mark'] == 'fuck'
