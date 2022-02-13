bc793121a5d4591a5e907ac8cf81906d6589c353
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 19:43:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ tab_Li_kt2 = pd.merge(tab_Li_kt2, tab_31, how = 'inner', on = ['Dn'])
 tab_Li_kt2.dropna(subset=['Fz'], inplace=True)
 tab_Li_kt2['mark'] = np.nan
 st.write(tab_Li_kt2)
-if tab_Li_kt2['Fz'] <= 100:
+if tab_Li_kt2['Fz_21'] <= 100:
     tab_Li_kt2['mark'] == 'yepp'
 else:
     tab_Li_kt2['mark'] == 'fuck'
