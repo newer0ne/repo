cc4546dc88c8428eaede520057f584274e716deb
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 16:23:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ st.write(tab31)
 
 uploaded_file2 = st.file_uploader("Зафгрузка тестовая")
 if uploaded_file2 is not None:
-    B = pd.read_excel(uploaded_file2, sheet_name=0, dtype={'Lisega' = str})
+    B = pd.read_excel(uploaded_file2, sheet_name=0, dtype={'Lisega': str})
     st.write(B)
 #    B = pd.merge(B, tab21, on = ['Dn'])
 #    B = pd.merge(B, tab31, on = ['Dn'])
