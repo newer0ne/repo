eb2e919b0bc85298c3038fa52a627bfa02b9a925
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-13 21:49:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,14 @@ tab_31 = pd.DataFrame(rows_31)
 tab_Li_kt21 = pd.merge(tab_Li, tab_21, how = 'inner', on = ['Dn'])
 tab_Li_kt21.dropna(subset=['Fz'], inplace=True)
 tab_Li_kt21 = tab_Li_kt21[tab_Li_kt21['Fz'] <= tab_Li_kt21['Fz_21']]
-st.write(tab_Li_kt21)
 tab_Li_kt21_drop = tab_Li_kt21[['Lisega','mark_21']]
 st.write(tab_Li_kt21_drop)
+
+tab_Li_kt31 = pd.merge(tab_Li, tab_31, how = 'inner', on = ['Dn'])
+tab_Li_kt31.dropna(subset=['Fz'], inplace=True)
+tab_Li_kt31 = tab_Li_kt31[tab_Li_kt31['Fz'] <= tab_Li_kt31['Fz_31']]
+tab_Li_kt31_drop = tab_Li_kt31[['Lisega','mark_31']]
+st.write(tab_Li_kt31_drop)
 
 #tab_Li_kt2 = pd.merge(tab_Li_kt2, tab_31, how = 'inner', on = ['Dn'])
 
