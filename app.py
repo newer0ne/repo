69a4dcfae26c583cb9263a9a292a8d6e85758e48
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-16 20:28:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ add_selectbox = st.sidebar.selectbox("Выберите базу данных д�
 if st.sidebar.button('Просмотреть'):
     if add_selectbox == "АЭС АККУЮ":
         st.sidebar.write(tab)
-        title = st.sidebar.text_input('AKKU', 'Lisega','KT2')
+        title = st.sidebar.text_input()
         st.sidebar.write('The current movie title is', title)
     if add_selectbox == "Курская АЭС":
         st.sidebar.write(tab_Li)
