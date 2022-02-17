6db52fcd81c0abcc7080917324682c763a85a4d8
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-17 12:33:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if uploaded_file2 is not None:
     B = pd.merge(B, tab_Li, how = 'left', on = ['Lisega'])
     st.write('Соответствие опор запрашиваемых в ведомости ОПС на Курскую АЭС. ',
              '**Развернуть** таблицу на весь экран можно кнопкой, находящейся **в правом верхнем углу** таблицы.')
-    st.write(B.drop('Lisega product group', axis=1)
+    st.write(B.drop('Lisega product group', axis=1))
 
     def to_excel(df):
         output = BytesIO()
