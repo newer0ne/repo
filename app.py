39b7b186906a04d5d7df3d9b6cfefc1afcfb1461
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-02-19 13:18:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -134,6 +134,6 @@ tab_Li['Li type'] = tab_Li['Lisega'].str[:2]
 tab_Li['Li diam class'] = tab_Li['Lisega'].str[2:4]
 tab_Li['Li temp class'] = tab_Li['Lisega'].str[4:6]
 st.write(tab_Li)
-№tab_Li_61 = pd.Dataframe(tab_Li['Li type'] == '61') 
+#tab_Li_61 = pd.Dataframe(tab_Li['Li type'] == '61') 
 tab_Li_61.loc[tab_Li['Li type'] == 61] 
 st.write(tab_Li_61)
