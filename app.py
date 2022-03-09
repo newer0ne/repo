a120ea934cc8aa4e947bdde593e4ed7d5e12c11b
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-09 14:52:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ with st.expander("Каталог KT2"):
     st.write(show_CatKT2.sort_values(by=['KT2_mark', 'KT2_diam']))
     title_KT2_1 = st.text_input('Поле ввода маркировки KT2 для проверки')
     if title_KT2_1 is not None:
-        st.write(show_CatKT2.loc[show_CatKT2['KT2_mark', 'Обозначение'] == title_KT2_1])
+        st.write(show_CatKT2.loc[show_CatKT2['KT2_mark'] == title_KT2_1])
     title_KT2_2 = st.text_input('Поле ввода Обозначения KT2 для проверки')
     if title_KT2_2 is not None:
         st.write(show_CatKT2.loc[show_CatKT2['Обозначение'] == title_KT2_2])
