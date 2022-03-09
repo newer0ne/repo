8c010b9d4b70f6b4eceae12681c1d4c7a11c56dd
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-09 14:41:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ with st.expander("Каталог Lisega"):
     title_Li = st.text_input('Поле ввода кода Lisega для проверки')
     if title_Li is not None:
         st.write(title_Li)
+        show_CatLi.loc[show_CatLi['Lisega'] == title_Li] 
     
 with st.expander("Каталог KT2"):
     #st.header('Каталог KT2')
