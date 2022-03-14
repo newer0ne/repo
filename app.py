976e3c52452f423cc47f030ee32aecb2f8e49e4c
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-14 11:10:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ with st.expander("Каталог Lisega"):
     
 with st.expander("Каталог KT2"):
     #st.header('Каталог KT2')
-    show_CatKT2 = CatKT2[['Lisega', 'AKU_mark', 'KT2_mark', 'Обозначение', 'Наименование', 'KT2_diam', 'KT2_mass', 'KT2_Fz_100']]
+    show_CatKT2 = CatKT2[['Lisega', 'AKU', 'KT2_mark', 'Обозначение', 'Наименование', 'KT2_diam', 'KT2_mass', 'KT2_Fz_100']]
     st.write(show_CatKT2.sort_values(by=['KT2_mark', 'KT2_diam']))
     title_KT2_1 = st.text_input('Поле ввода Маркировки KT2 для проверки')
     st.write(show_CatKT2.loc[show_CatKT2['KT2_mark'] == title_KT2_1])
