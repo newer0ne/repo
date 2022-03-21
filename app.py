d7bf1e78d084eda3884a6a2428719e1c1709788f
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-21 12:28:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ st.sidebar.header('Модуль классификации ведомостей 
 
 uploaded_file = st.sidebar.file_uploader("Загрузка ведомости опор в формате .xls (Нужно удалить первые два скрытых столбца. Таблица должна начинаться со столбца **Код KKS**)")
 if uploaded_file is not None:
-    st.write(uploaded_file['name'])
+    st.write(uploaded_file[['name']])
     A = pd.read_excel(uploaded_file, sheet_name="Sheet1")
     final = pd.merge(A, CatAKU, how = 'left', on = ['Note'])
     st.write('Соответствие опор запрашиваемых в ведомости ОПС на АЭС АККУЮ. ',
