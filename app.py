05702317723016c149299ee6e0454e5ee8debe30
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-21 11:58:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def to_excel(df):
     df.to_excel(writer, index=False, sheet_name='Sheet1')
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.00'})
+    format1 = workbook.add_format({'num_format': '0,00'})
     worksheet.set_column('A:A', None, format1)
     writer.save()
     processed_data = output.getvalue()
