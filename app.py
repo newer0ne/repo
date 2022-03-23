88fa41d3d3b5217c979f0a8b5afb3d374be15108
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-23 11:10:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,11 +84,11 @@ with st.expander("Каталог Lisega"):
 with st.expander("Каталог KT2"):
     #st.header('Каталог KT2')
     show_CatKT2 = CatKT2[['Note', 'AKU', 'Маркировка_KT2', 'Обозначение_KT2', 'Наименование_KT2', 'KT2_diam', 'Масса_KT2', 'Нагрузка_KT2']]
-    st.write(show_CatKT2.sort_values(by=['KT2_mark', 'KT2_diam']))
+    st.write(show_CatKT2.sort_values(by=['Маркировка_KT2', 'KT2_diam']))
     title_KT2_1 = st.text_input('Поле ввода Маркировки KT2 для проверки')
-    st.write(show_CatKT2.loc[show_CatKT2['KT2_mark'] == title_KT2_1])
+    st.write(show_CatKT2.loc[show_CatKT2['Маркировка_KT2'] == title_KT2_1])
     title_KT2_2 = st.text_input('Поле ввода Обозначения KT2 для проверки')
-    st.write(show_CatKT2.loc[show_CatKT2['Обозначение'] == title_KT2_2])
+    st.write(show_CatKT2.loc[show_CatKT2['Обозначение_KT2'] == title_KT2_2])
     
 # Выбрасываем лишние стлобцы из каталогов и склеиваем их по средствам pd.merge
 #st.header('Таблица соответствия')
