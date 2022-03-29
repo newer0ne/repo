ecbf1c6c8ee28289472ca891f07a96a4b5d16c70
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-03-29 10:40:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ st.sidebar.write("3. Определяемый столбец дожен имет
 uploaded_file3 = st.sidebar.file_uploader("Область загрузки")
 if uploaded_file3 is not None:
     st.write("Filename: ", uploaded_file3.name)
-    С = pd.read_excel(uploaded_file3)               #sheet_name = "Лист1" "Sheet1" or dtype = {'Note': str}
+    С = pd.read_excel(uploaded_file3, sheet_name = "Лист1" | "Sheet1")              #sheet_name = "Лист1" "Sheet1" or dtype = {'Note': str}
     st.write(С)
     final = pd.merge(С, Cat, how = 'left', on = ['Note'])
     st.write('Соответствие опор запрашиваемых в ведомости.',
