ea587ba37316872d8d85d4536a820e569aae2140
newer0ne <93090004+newer0ne@users.noreply.github.com>
2022-06-13 21:56:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-
+g_login = GoogleAuth()
+g_login.LocalWebserverAuth()
+drive = GoogleDrive(g_login)
 
 
 conn = connect()                                                        # Create a connection object.
